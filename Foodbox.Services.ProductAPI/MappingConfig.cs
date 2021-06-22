111528c4cd26426400cc8b3ca45ac6655d8f203b
Isaac Oduh <ufuomaoduh@gmail.com>
2021-06-22 10:16:46 +0100

    Add Automapper Configuration

---
+++ Foodbox.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Foodbox.Services.ProductAPI.Models;
+using Foodbox.Services.ProductAPI.Models.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foodbox.Services.ProductAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDTO, Product>();
+                config.CreateMap<Product, ProductDTO>();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
--- Foodbox.Services.ProductAPI/Startup.cs
+++ Foodbox.Services.ProductAPI/Startup.cs
@@ -1,3 +1,4 @@
+using AutoMapper;
 using Foodbox.Services.ProductAPI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -29,6 +30,10 @@ namespace Foodbox.Services.ProductAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+            services.AddSingleton(mapper);
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
