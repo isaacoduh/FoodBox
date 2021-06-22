e30f0df5c5aed913bcffd2ba601bd4308f1a05d7
Isaac Oduh <ufuomaoduh@gmail.com>
2021-06-22 11:53:48 +0100

    Implement Base Service for Web Project

---
+++ Foodbox.Web/Services/BaseService.cs
@@ -0,0 +1,78 @@
+﻿using Foodbox.Web.Models;
+using Foodbox.Web.Services.IServices;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodbox.Web.Services
+{
+    public class BaseService : IBaseService
+    {
+        public ResponseDTO responseModel { get; set; }
+        public IHttpClientFactory httpClient { get; set; }
+
+        public BaseService(IHttpClientFactory httpClient)
+        {
+            this.responseModel = new ResponseDTO();
+            this.httpClient = httpClient;
+        }
+
+        public async  Task<T> SendAsync<T>(ApiRequest apiRequest)
+        {
+            try
+            {
+                var client = httpClient.CreateClient("FoodboxAPI");
+                HttpRequestMessage message = new HttpRequestMessage();
+                message.Headers.Add("Accept", "application/json");
+                message.RequestUri = new Uri(apiRequest.Url);
+                client.DefaultRequestHeaders.Clear();
+                if(apiRequest.Data != null)
+                {
+                    message.Content = new StringContent(JsonConvert.SerializeObject(apiRequest.Data), Encoding.UTF8, "application/json");
+                }
+
+                HttpResponseMessage apiResponse = null;
+                switch (apiRequest.ApiType)
+                {
+                    case SD.ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case SD.ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case SD.ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+                apiResponse = await client.SendAsync(message);
+                var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                var apiResponseDTO = JsonConvert.DeserializeObject<T>(apiContent);
+                return apiResponseDTO;
+            }catch(Exception e)
+            {
+                var DTO = new ResponseDTO
+                {
+                    Message = "Error",
+                    Errors = new List<string> { Convert.ToString(e.Message) },
+                    IsSuccess = false,
+                };
+                var res = JsonConvert.SerializeObject(DTO);
+                var apiResponseDTO = JsonConvert.DeserializeObject<T>(res);
+                return apiResponseDTO;
+            }
+        }
+        public void Dispose()
+        {
+            GC.SuppressFinalize(true);
+        }
+
+        
+    }
+}
---
+++ Foodbox.Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,14 @@
+﻿using Foodbox.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foodbox.Web.Services.IServices
+{
+    public interface IBaseService
+    {
+        ResponseDTO responseModel { get; set; }
+        Task<T> SendAsync<T>(ApiRequest apiRequest);
+    }
+}
