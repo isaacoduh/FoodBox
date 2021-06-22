4b5f2148064a64f3cf2645842ca97d106c9e5258
Isaac Oduh <ufuomaoduh@gmail.com>
2021-06-22 10:12:11 +0100

    Add Product Repository Interface

---
+++ Foodbox.Services.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using Foodbox.Services.ProductAPI.Models.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foodbox.Services.ProductAPI.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductDTO>> GetProducts();
+        Task<ProductDTO> GetProductById(int productId);
+        Task<ProductDTO> CreateUpdateProduct(ProductDTO productDTO);
+        Task<bool> DeleteProduct(int productId);
+    }
+}
