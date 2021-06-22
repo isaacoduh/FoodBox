44ec17836cf0b5054bd78c589d73e713f241de7d
Isaac Oduh <ufuomaoduh@gmail.com>
2021-06-22 10:09:26 +0100

    Add ProductDTO and Response DTO

---
+++ Foodbox.Services.ProductAPI/Models/Dto/ResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foodbox.Services.ProductAPI.Models.Dto
+{
+    public class ResponseDTO
+    {
+        public bool IsSuccess { get; set; } = true;
+        public object Result { get; set; }
+        public string Message { get; set; }
+        public List<string> Errors { get; set; }
+    }
+}
