34bbe9677a71efa17087be383d2ba0738d8fb206
Isaac Oduh <ufuomaoduh@gmail.com>
2021-06-22 11:27:03 +0100

    Add Product and Response DTO to web project

---
+++ Foodbox.Web/Models/ResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foodbox.Web.Models
+{
+    public class ResponseDTO
+    {
+        public bool IsSuccess { get; set; } = true;
+        public object Result { get; set; }
+        public string Message { get; set; }
+        public List<string> Errors { get; set; }
+    }
+}
---
+++ Foodbox.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using Foodbox.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foodbox.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<T> GetAllProductsAsync<T>();
+        Task<T> GetProductByIdAsync<T>(int id);
+        Task<T> CreateProductAsync<T>(ProductDTO productDTO);
+        Task<T> UpdateProductAsync<T>(ProductDTO productDTO);
+        Task<T> DeleteProductAsync<T>(int id);
+    }
+}
