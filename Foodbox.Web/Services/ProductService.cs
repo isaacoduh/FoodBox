7bbf49bda939bda2b274a69b369f823f7b77ce3c
Isaac Oduh <ufuomaoduh@gmail.com>
2021-06-24 15:57:48 +0100

    Create Product Service in Web Project, Configure the Http Client in the Web Project Startup File

---
+++ Foodbox.Web/Services/ProductService.cs
@@ -0,0 +1,73 @@
+﻿using Foodbox.Web.Models;
+using Foodbox.Web.Services.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace Foodbox.Web.Services
+{
+    public class ProductService : BaseService, IProductService
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public ProductService(IHttpClientFactory clientFactory) : base(clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        public async Task<T> CreateProductAsync<T>(ProductDTO productDTO)
+        {
+            return await this.SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = productDTO,
+                Url = SD.ProductAPIBase + "/api/products",
+                AccessToken = ""
+            });
+        }
+
+        public async Task<T> DeleteProductAsync<T>(int id)
+        {
+            return await this.SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.DELETE,
+                Url = SD.ProductAPIBase + "/api/products/" + id,
+                AccessToken = ""
+            });
+        }
+
+
+        public async Task<T> GetAllProductsAsync<T>()
+        {
+            return await this.SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = SD.ProductAPIBase + "/api/products",
+                AccessToken = ""
+            });
+        }
+
+        public async Task<T> GetProductByIdAsync<T>(int id)
+        {
+            return await this.SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = SD.ProductAPIBase + "/api/products/" + id,
+                AccessToken = ""
+            });
+        }
+
+        public async Task<T> UpdateProductAsync<T>(ProductDTO productDTO)
+        {
+            return await this.SendAsync<T>(new ApiRequest()
+            {
+                ApiType = SD.ApiType.PUT,
+                Data = productDTO,
+                Url = SD.ProductAPIBase + "/api/products",
+                AccessToken = ""
+            });
+        }
+    }
+}
